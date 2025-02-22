1b5fbf7a4b9b4a02e5d46c3219d6ed3709d204a8
Erciyuan11 <891054582@qq.com>
2025-02-22 13:30:19 +0800

    配置状态机

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -13,13 +13,16 @@ public class EnemyController : MonoBehaviour
     public int damage = -1;
     public float changetime = 1.0f;
     int direction = 1;
+    public bool vertical = true;
     Rigidbody2D rigidbody2d;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changetime;
         currenthealth = maxhealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,17 +33,30 @@ public class EnemyController : MonoBehaviour
         {
             direction = -direction;
             timer = changetime;
+         
         }
     }
 
     void FixedUpdate()
     {
         Vector2 position = transform.position;
-        position.x = position.x + enemyspeed *direction * Time.deltaTime;
-        position.y = position.y + enemyspeed*direction * Time.deltaTime;
+    
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * enemyspeed * direction;
+            animator.SetFloat("MoveX", 0);
+            animator.SetFloat("MoveY", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * enemyspeed * direction;
+            animator.SetFloat("MoveY", 0);
+            animator.SetFloat("MoveX", direction);
+        }
+
         rigidbody2d.MovePosition(position);
 
-        
+
     }
 
      void OnCollisionEnter2D(Collision2D collision)
